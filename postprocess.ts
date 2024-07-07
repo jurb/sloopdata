95b31b85c26272e46a97370820066b927c964e57
Jurian Baas <jurian.baas@gmail.com>
2024-07-07 17:56:35 +0200

    try this

--- postprocess.ts
+++ postprocess.ts
@@ -5,6 +5,7 @@ const filename = Deno.args[0] // Same name as downloaded_filename
 
 console.log(`Processing ${filename}...`)
 
-const result = await unZipFromFile(filename, filename + '.csv')
+const result = await unZipFromFile(filename)
 const output = result ? 'File unzipped successfully' : 'Error unzipping'
 console.log(output)
+
