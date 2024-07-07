a1ba0201fd6e8d487ed2247e846eb8ceefb86127
Jurian Baas <jurian.baas@gmail.com>
2024-07-07 17:36:27 +0200

    ??

--- postprocess.ts
+++ postprocess.ts
@@ -1,11 +1,16 @@
+import { unZipFromFile } from "https://deno.land/x/zip@v1.1.0/unzip.ts";
 import { readCSV } from "jsr:@vslinko/csv";
+import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts'
 
-const f = await Deno.open("./bag_panden.csv");
-for await (const row of readCSV(f)) {
+// The filename is the first invocation argument
+const filename = Deno.args[0] // Same name as downloaded_filename
+
+const bag_panden_file = await unZipFromFile(filename, 'bag_panden.csv')
+
+for await (const row of readCSV(bag_panden_file)) {
     console.log("row:");
     for await (const cell of row) {
         console.log(`  cell: ${cell}`);
     }
 }
 
-f.close();
