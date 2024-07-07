b044c1a086b29affd43913f0108ae035d5231a0b
Jurian Baas <jurian.baas@gmail.com>
2024-07-07 17:42:51 +0200

    tja

--- postprocess.ts
+++ postprocess.ts
@@ -2,3 +2,8 @@
 const filename = Deno.args[0] // Same name as downloaded_filename
 
 console.log(`Processing ${filename}...`)
+
+
+import { decompress } from "https://deno.land/x/zip@v1.2.5/mod.ts";
+
+console.log(await decompress("bag.zip"))
