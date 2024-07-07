4769a314ef5263258d24e25af4371763b9d9313c
Jurian Baas <jurian.baas@gmail.com>
2024-07-07 17:53:07 +0200

    ...

--- postprocess.ts
+++ postprocess.ts
@@ -1,3 +1,5 @@
+import { unZipFromFile } from "https://deno.land/x/flat@0.0.15/src/zip.ts";
+
 // The filename is the first invocation argument
 const filename = Deno.args[0] // Same name as downloaded_filename
 
