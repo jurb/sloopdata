855d99b2489d907e9ab218ca6cfd20552b3979cd
Jurian Baas <jurian.baas@gmail.com>
2024-07-07 21:52:25 +0200

    leeeets go

--- postprocess.ts
+++ postprocess.ts
@@ -2,10 +2,79 @@ import { unZipFromFile } from "https://deno.land/x/flat@0.0.15/src/zip.ts";
 
 // The filename is the first invocation argument
 const filename = Deno.args[0] // Same name as downloaded_filename
+// const filename = 'bag.zip' // Same name as downloaded_filename
 
 console.log(`Processing ${filename}...`)
 
 const result = await unZipFromFile(filename)
-const output = result ? 'File unzipped successfully' : 'Error unzipping'
-console.log(output)
+// const output = result ? 'File unzipped successfully' : 'Error unzipping'
+// console.log(output)
 
+
+
+
+
+import { parse } from "jsr:@std/csv";
+import { writeJSON } from 'https://deno.land/x/flat/mod.ts'
+const rawContent = Deno.readFileSync(result)
+const decodedContent = new TextDecoder().decode(rawContent)
+// Parse it
+const parsed = await parse(decodedContent, {
+    skipFirstRow: true,
+    strip: true,
+})
+
+import administrativeAreasPropertiesFlat from "./administrativeAreas.json" with { type: "json" }
+
+
+
+// const stadsdelen = await fetch('https://api.data.amsterdam.nl/v1/gebieden/stadsdelen?page_size=999999').then(res => res.json())
+// console.log('stadsdelen: ' + stadsdelen._embedded.stadsdelen.length)
+
+// const wijken = await fetch('https://api.data.amsterdam.nl/v1/gebieden/wijken?page_size=999999').then(res => res.json())
+// console.log('wijken: ' + wijken._embedded.wijken.length)
+
+// const buurten = await fetch('https://api.data.amsterdam.nl/v1/gebieden/buurten?page_size=999999').then(res => res.json())
+// console.log('buurten: ' + buurten._embedded.buurten.length)
+
+const getBuurten_weesp = () => {
+    const weesp = administrativeAreasPropertiesFlat.stadsdelen.find((d) => d.properties.naam === "Weesp");
+    const wijken_weesp = administrativeAreasPropertiesFlat.wijken.filter(
+        (d) => d.properties.ligtInStadsdeelId === weesp.properties.identificatie
+    );
+    return administrativeAreasPropertiesFlat.buurten.filter((d) =>
+        wijken_weesp.map((e) => e.properties.identificatie).includes(d.properties.ligtInWijkId)
+    );
+}
+
+const buurten_weesp = getBuurten_weesp()
+
+const bag_panden = parsed
+    .filter(
+        (d) =>
+            !buurten_weesp.map((e) => e.properties.identificatie).includes(d.Ligtinbuurtid)
+    )
+    .map((d) => ({
+        geo: administrativeAreasPropertiesFlat.buurten.find(
+            (e) => e.properties.identificatie === d.Ligtinbuurtid
+        ),
+        ...d
+    }))
+
+const bag_panden_gesloopt = bag_panden
+    .filter((d) => d.Statusomschrijving === "Pand gesloopt")
+    .map((d) => d)
+    .map(({ Geometrie, index, ...rest }) => ({
+        ...rest
+    }))
+    .map((d) => ({
+        stadsdeel: d?.geo?.properties.ligtInStadsdeel?.naam,
+        wijk: d?.geo?.properties.ligtInWijk?.naam,
+        buurt: d?.geo?.properties.naam,
+        ...d
+    }))
+    .map(({ geo, index, ...rest }) => ({
+        ...rest
+    }))
+
+console.log(bag_panden_gesloopt)
